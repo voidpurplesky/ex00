74482d61b7291f109fcc69eea335c2fe8d55edf5
voidname <voidpurplesky@gmail.com>
2024-10-07 17:34:21 +0900

    first commit

---
+++ ex00/src/test/java/org/zerock/sample/DataSourceTests.java
@@ -0,0 +1,36 @@
+package org.zerock.sample;
+
+import java.sql.Connection;
+
+import javax.sql.DataSource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@Log4j
+public class DataSourceTests {
+
+	@Setter(onMethod_ = @Autowired)
+	//@Autowired
+	private DataSource dataSource;
+	
+	@Test
+	public void testConnection() {
+		try {
+			Connection con = dataSource.getConnection();
+			//System.out.println(con);
+			log.info(con);
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+	}
+}
